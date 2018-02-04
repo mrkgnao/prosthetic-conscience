3e2bb4f2454daceffa350b79ac70269bac254c4c
Soham Chowdhury <chow.soham@gmail.com>
2018-02-04 20:47:56 +0530

    [setup] bare dir

---
+++ shell.nix
@@ -0,0 +1,94 @@
+let 
+  rien = import ../rien/rien.nix {
+    packageName = "hello-world";
+    packagePath = ./.;
+
+    # Instead of using <nixpkgs>, use a lock-file to stick to
+    # a particular `nixpkgs` commit.
+    nixpkgsLock = ./nixpkgs.json;
+
+    ghcVersion = "ghc822";
+
+    overrides = rec {
+      jailbreak = [ "cabal-helper" "ghc-mod" "liquidhaskell" ];
+      skipHaddock = justStaticExecutables;
+      skipTests = [ "cabal-helper" "ghc-mod" "tweet-hs" ];
+      justStaticExecutables = [ 
+        "brittany" 
+        "hpack"
+      ];
+    };
+  };
+
+in
+  rien.shell {
+    # Generate Hoogle documentation?
+    wantHoogle = true;
+
+    # Haskell dependencies
+    deps = hsPkgs: with hsPkgs; [
+      brittany
+      hindent
+      hpack
+      ghc-mod
+
+      aeson
+      attoparsec
+      authenticate-oauth
+      base
+      bytestring
+      case-insensitive
+      conduit
+      conduit-combinators
+      conduit-extra
+      containers
+      data-default
+      directory
+      exceptions
+      filepath
+      http-client
+      http-conduit
+      http-types
+      lens
+      network-uri
+      process
+      resourcet
+      scalpel
+      stm-conduit
+      taggy
+      taggy-lens
+      tagsoup
+      template-haskell
+      text
+      time
+      transformers
+      transformers-base
+      twitter-types
+      twitter-types-lens
+      wreq
+
+      scientific
+      hashable
+      rel8
+      postgresql-simple
+      sqlite-simple
+      opaleye
+
+      beam-core
+      beam-sqlite
+      beam-postgres
+      beam-migrate
+      beam-migrate-cli
+    ];
+
+    # Optionally, also add sets of related packages that are
+    # commonly used together.
+    depSets = hsPkgs: with (rien.package-sets hsPkgs); [ ];
+
+    # Native dependencies
+    nativeDeps = pkgs: with pkgs; [
+      # llvm
+      postgresql
+      sqlite
+    ];
+  }
